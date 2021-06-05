033a8aa0bcabe7b25ee447a4649ff528888c60b0
Mo <58116522+Metropass@users.noreply.github.com>
2021-06-05 16:39:12 -0400

    Added Initial Python Code

---
+++ playground.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import psutil
+
+
+#s = ['SLUS', 'SCES']
+#df = pd.read_csv('PS2-GAMEID-TITLE-MASTER.csv', sep=';')
+#print(df[df['GameID'].str.contains('|'.join(s))])
+
+
+class PS2Data:
+    def __init__(self):
+        self.__file = 'PS2-GAMEID-TITLE-MASTER.csv'
+        self.__df = pd.read_csv('PS2-GAMEID-TITLE-MASTER.csv', sep=';')
+    def searchByRegion(value):
+        #Values: 1 - Americas, 2 - Europe, 3 - Japan
+        try:
+            q = None
+            if value == 1:
+                s = ['SLUS', 'SCES']
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                print(q)
+            elif value == 2:
+                s = ['SCES', 'SLES']
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                print(q)
+            elif value == 3:
+                s = ['SCPS', 'SLPS']
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                print(q)
+            return q
+        except ValueError:
+            print("NOT IN RANGE")
+    def searchByID(IDValue):
+        df_search = self.__df[self.__df['GameID'] == IDValue]
+        if df_search.empty:
