723e90fc83223d4adc0c684e2e895f0f2f0ecd17
Mo <58116522+Metropass@users.noreply.github.com>
2021-06-05 16:40:24 -0400

    Add files via upload

--- playground.py
+++ playground.py
@@ -2,15 +2,11 @@ import pandas as pd
 import psutil
 
 
-#s = ['SLUS', 'SCES']
-#df = pd.read_csv('PS2-GAMEID-TITLE-MASTER.csv', sep=';')
-#print(df[df['GameID'].str.contains('|'.join(s))])
-
-
 class PS2Data:
     def __init__(self):
         self.__file = 'PS2-GAMEID-TITLE-MASTER.csv'
         self.__df = pd.read_csv('PS2-GAMEID-TITLE-MASTER.csv', sep=';')
+
     def searchByRegion(value):
         #Values: 1 - Americas, 2 - Europe, 3 - Japan
         try:
@@ -30,6 +26,12 @@ class PS2Data:
             return q
         except ValueError:
             print("NOT IN RANGE")
+
     def searchByID(IDValue):
         df_search = self.__df[self.__df['GameID'] == IDValue]
         if df_search.empty:
+            print('The search returned empty')
+            return None
+        else:
+            print(df_search)
+            return df_search
