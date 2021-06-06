bce5dae595512fed0816ffa41c218cb03904267f
Mo <58116522+Metropass@users.noreply.github.com>
2021-06-06 16:38:35 -0400

    Fixed self assigning issues, syntax fixes with searches

--- playground.py
+++ playground.py
@@ -7,27 +7,27 @@ class PS2Data:
         self.__file = 'PS2-GAMEID-TITLE-MASTER.csv'
         self.__df = pd.read_csv('PS2-GAMEID-TITLE-MASTER.csv', sep=';')
 
-    def searchByRegion(value):
+    def searchByRegion(self, value):
         #Values: 1 - Americas, 2 - Europe, 3 - Japan
         try:
             q = None
             if value == 1:
                 s = ['SLUS', 'SCES']
-                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s))]
                 print(q)
             elif value == 2:
                 s = ['SCES', 'SLES']
-                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s))]
                 print(q)
             elif value == 3:
                 s = ['SCPS', 'SLPS']
-                q = self.__df[self.__df['GameID'].str.contains('|'.join(s)])
+                q = self.__df[self.__df['GameID'].str.contains('|'.join(s))]
                 print(q)
             return q
         except ValueError:
             print("NOT IN RANGE")
 
-    def searchByID(IDValue):
+    def searchByID(self,IDValue):
         df_search = self.__df[self.__df['GameID'] == IDValue]
         if df_search.empty:
             print('The search returned empty')
@@ -35,3 +35,8 @@ class PS2Data:
         else:
             print(df_search)
             return df_search
+
+
+
+p = PS2Data()
+p.searchByID('SCUS-97199')
